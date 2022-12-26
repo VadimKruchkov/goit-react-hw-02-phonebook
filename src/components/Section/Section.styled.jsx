dd005feae0b903ae9b31886b7b3156860d84ec69
VadimKruchkov <Vtradecapital@gmail.com>
2022-12-26 16:33:34 +0200

    add task

---
+++ src/components/Section/Section.styled.jsx
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+
+export const SectionItem = styled.section`
+  width: 360px;
+  text-align: center;
+  margin-left: 8px;
+`;
+
+export const SectionTitle = styled.h2`
+  font-weight: 700;
+  margin: 0;
+  padding: 16px;
+  text-align: left;
+  font-family: 'Roboto', sans-serif;
+`;
